749b0b191efbf465e962df32345f6c939906e10b
Maya Yanai <mayayanai@gmail.com>
2022-05-30 14:39:02 +0300

    finds first ip which isn't 127.0.0.1

--- main.py
+++ main.py
@@ -12,7 +12,10 @@ def print_interfaces():
 def print_fisrt_ip():
     for ifaceName in interfaces():
         addresses = [i['addr'] for i in ifaddresses(ifaceName).setdefault(AF_INET, [{'addr': 'No IP addr'}])]
-        print(' '.join(addresses))
+    for address in addresses:
+        if address != "127.0.0.1":
+            print(address)
+            break
 
 
 if __name__ == '__main__':
