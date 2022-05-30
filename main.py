823b7cf2eaf18f8f6e55f434a8e4e39b921ac133
Maya Yanai <mayayanai@gmail.com>
2022-05-30 14:10:25 +0300

    added datetime

---
+++ main.py
@@ -0,0 +1,11 @@
+from datetime import datetime
+
+
+def print_datetime():
+    print(datetime.utcnow())
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_datetime()
+
