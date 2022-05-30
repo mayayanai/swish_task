ff57569b3c2aa38c8c90faa0acca6fc889d19b41
Maya Yanai <mayayanai@gmail.com>
2022-05-30 14:32:30 +0300

    added interfaces and ip list

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 from datetime import datetime
-
+from netifaces import interfaces, ifaddresses, AF_INET
+import netifaces
 
 def print_datetime():
     print(datetime.utcnow())
 
+def print_interfaces():
+    print(netifaces.interfaces())
 
-# Press the green button in the gutter to run the script.
+
+def print_fisrt_ip():
+    for ifaceName in interfaces():
+        addresses = [i['addr'] for i in ifaddresses(ifaceName).setdefault(AF_INET, [{'addr': 'No IP addr'}])]
+        print(' '.join(addresses))
+
+
 if __name__ == '__main__':
     print_datetime()
-
+    print_interfaces()
+    print_fisrt_ip()
