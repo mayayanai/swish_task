d318a92f28dc0b7bc4f98d2c0d21835bca3149a1
Maya Yanai <mayayanai@gmail.com>
2022-05-30 17:06:03 +0300

    CPU info

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from datetime import datetime
 from netifaces import interfaces, ifaddresses, AF_INET
 import netifaces
 import requests
+import platform
+import multiprocessing
 
 def print_datetime():
     print(datetime.utcnow())
@@ -22,9 +24,16 @@ def print_external_ip():
     response = requests.get('https://ident.me',timeout=10)
     print(response.content.decode("utf-8"))
 
+def print_CPU_model():
+    print(platform.processor())
+
+def print_core_number():
+    print(multiprocessing.cpu_count())
 
 if __name__ == '__main__':
     print_datetime()
     print_interfaces()
     print_fisrt_ip()
     print_external_ip()
+    print_CPU_model()
+    print_core_number()
